e38c82fb0d7d3267a54f58f9e0135fc01e6be199
torfoz <tord.fosse@gmail.com>
2024-11-04 15:34:55 +0100

    practice 10

--- Practice10/Event.java
+++ Practice10/Event.java
@@ -1,3 +1,63 @@
-public class Event {
+package Practice10;
 
+public class Event {
+  private int eventId;
+  private String eventName;
+  private String eventLocation;
+  private String eventOrganizer;
+  private long eventDate;
+  private String eventType;
+
+  public Event(int eventId, String eventName, String eventLocation, String eventOrganizer, long eventDate, String eventType) {
+    this.eventId = eventId;
+    this.eventName = eventName;
+    this.eventLocation = eventLocation;
+    this.eventOrganizer = eventOrganizer;
+    this.eventDate = eventDate;
+    this.eventType = eventType;
+  }
+
+  public int getEventId() {
+    return this.eventId;
+  }
+
+  public String getEventName() {
+    return this.eventName;
+  }
+
+  public String getEventLocation() {
+    return this.eventLocation;
+  }
+
+  public String getEventOrganizer() {
+    return this.eventOrganizer;
+  }
+
+  public long getEventDate() {
+    return this.eventDate;
+  }
+
+  public String getEventType() {
+    return this.eventType;
+  }
+
+  public String toString() {
+    return eventId + " " + eventName + " " + eventLocation + " " + eventOrganizer + " " + eventDate + " " + eventType;
+  }
+
+  public static void main(String[] args) {
+    EventRegister eventRegister = new EventRegister();
+    eventRegister.addEvent(1, "name1", "location1", "organizer1", 202101011900L, "type1");
+    eventRegister.addEvent(2, "name2", "location1", "organizer2", 202101011800L, "type2");
+    eventRegister.addEvent(3, "name3", "location3", "organizer3", 202101031500L, "type3");
+    eventRegister.addEvent(4, "name4", "location2", "organizer4", 202101041700L, "type4");
+
+    System.out.println(eventRegister.getEventByLocation("location1"));
+    System.out.println(eventRegister.getEventByDate(20210101));
+    System.out.println(eventRegister.getEventByDateInterval(202101010000L, 202101020000L));
+    System.out.println(eventRegister.getSortedEventsByLocation());
+    System.out.println(eventRegister.getSortedEventsByDate());
+    System.out.println(eventRegister.getSortedEventsByType("type1"));
+
+  }
 }
--- Practice10/EventRegister.java
+++ Practice10/EventRegister.java
@@ -1,4 +1,57 @@
-public class EventRegister {
+package Practice10;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class EventRegister {
+  private List<Event> events = new ArrayList<>();
+
+
+  public void addEvent(int eventId, String eventName, String eventLocation, String eventOrganizer, long eventDate, String eventType) {
+    events.add(new Event(eventId, eventName, eventLocation, eventOrganizer, eventDate, eventType));
+  }
+
+  public List<Event> getEventByDate(int date) {
+    String dateStr = Integer.toString(date);
+    String firstEightDigits = dateStr.substring(0, 8);
+
+    return events.stream()
+        .filter(event -> Long.toString(event.getEventDate()).substring(0, 8).equals(firstEightDigits))
+        .sorted(Comparator.comparing(e -> Long.toString(e.getEventDate()).substring(8)))
+        .collect(Collectors.toList());
+  }
+
+  public List<Event> getEventByDateInterval(long startDate, long endDate) {
+    return events.stream()
+        .filter(event -> event.getEventDate() >= startDate && event.getEventDate() <= endDate)
+        .sorted(Comparator.comparingLong(Event::getEventDate))
+        .collect(Collectors.toList());
+  }
+
+  public List<Event> getEventByLocation(String location) {
+    return events.stream()
+        .filter(event -> event.getEventLocation().equals(location))
+        .collect(Collectors.toList());
+  }
+
+  public List<Event> getSortedEventsByDate() {
+    return events.stream()
+        .sorted(Comparator.comparingLong(Event::getEventDate))
+        .collect(Collectors.toList());
+  }
+
+  public List<Event> getSortedEventsByType(String type) {
+    return events.stream()
+        .filter(event -> event.getEventType().equals(type))
+        .collect(Collectors.toList());
+  }
+
+  public List<Event> getSortedEventsByLocation() {
+    return events.stream()
+        .sorted(Comparator.comparing(Event::getEventLocation))
+        .collect(Collectors.toList());
+  }
 
 }
